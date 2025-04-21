d212aac05b000e106378e8eb353a86ae8aa5152e
Bouchentouf Zakaria <119014719+BouchantoufZakaria@users.noreply.github.com>
2025-04-21 23:25:36 +0100

    - add a new logger library - add the get user data and decide if the user has already login before or not deferently in each platform

--- composeApp/src/commonMain/kotlin/com/zako/webetu/navigation/presentation/destinations/Home.kt
+++ composeApp/src/commonMain/kotlin/com/zako/webetu/navigation/presentation/destinations/Home.kt
@@ -8,5 +8,7 @@ import com.zako.webetu.navigation.model.MainGraph
 fun NavGraphBuilder.mainGraph(
     navHostController: NavHostController,
 ) {
-    composable<MainGraph> { MainGraphRoot() }
+    composable<MainGraph> {
+        MainGraphRoot()
+    }
 }
